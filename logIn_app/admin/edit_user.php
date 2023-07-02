2f41f71f41a01405ced0435c9b39aff7be38f5e0
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2023-07-03 01:45:07 +0300

    admin search , edit

---
+++ logIn_app/admin/edit_user.php
@@ -0,0 +1,73 @@
+<?php
+    session_start();
+    if(isset($_SESSION['user'])){
+
+        if($_SESSION['user']->role === 'admin'){
+
+
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "m_elshaarawy";
+
+            $conn = new PDO("mysql: host=$servername;dbname=$dbname",$username,$password);
+
+            if(isset($_SESSION['user_id'])){
+                $user = $conn->prepare("SELECT * FROM users WHERE id=:ID");
+                $user->bindParam("ID",$_SESSION['user_id']);
+                $user->execute();
+                $user=$user->fetchObject();
+
+                echo '<form action="" method="post">
+                Name : <input type="text" name="text" value="'.$user->name.'" require>
+                <br>
+                Age  : <input type="date" name="date" value="'.$user->age.'" require>
+                <br>';
+                echo'Status :<select name="activate" >';
+                if($user->activate === 1){
+                    echo'<option value="'.$user->activate.'">Active</option>';
+                }else{
+                    echo'<option value="'.$user->activate.'">InActive</option>';
+                }
+                echo'<option value="0">InActive</option>
+                <option value="1">Active</option>
+                </select>
+                <br>
+                <a href="index.php"> home</a>
+                <button type="submit" name="update" value="'.$user->id.'"> update </button>
+
+                </form>';
+
+            }
+
+            if(isset($_POST['update'])){
+                $update = $conn->prepare("UPDATE users SET name=:Name, age=:Age , activate=:Act WHERE id=:ID");
+                $update->bindParam("ID",$_SESSION['user_id']);
+                $update->bindParam("Name",$_POST['text']);
+                $update->bindParam("Age",$_POST['date']);
+                $update->bindParam("Act",$_POST['activate']);
+                $update->execute();
+                echo "<div style=\" color:green; \">  successfully updated </dev>";
+                header("Refresh:1;url=edit_user.php");
+
+            }
+
+
+
+
+
+
+        }else{
+            session_unset();
+            session_destroy();
+            header("Location:http://localhost/practice/logIn_app/login.php");
+            die("");
+        }
+    }else{
+        session_unset();
+        session_destroy();
+        header("Location:http://localhost/practice/logIn_app/login.php");
+        die("");
+    }
+    ?>
+
--- logIn_app/admin/search.php
+++ logIn_app/admin/search.php
@@ -8,14 +8,16 @@
             <button type="submit" name="searchB"> search </button>
             <a href="index.php"> home</a>
             </form>';
-    
+
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "m_elshaarawy";
+
+            $conn = new PDO("mysql: host=$servername;dbname=$dbname",$username,$password);
+
             if(isset($_GET['searchB'])){
-                $servername = "localhost";
-                $username = "root";
-                $password = "";
-                $dbname = "m_elshaarawy";
-    
-                $conn = new PDO("mysql: host=$servername;dbname=$dbname",$username,$password);
+
                 $SEARCH =$conn->prepare("SELECT * FROM users WHERE name LIKE :Name OR email LIKE :Name");
                 $Sresult = '%'.$_GET['search'].'%';
                 $SEARCH->bindParam("Name",$Sresult);
@@ -27,11 +29,15 @@
                     echo '<tr>';
                     echo '<th> Name </th>';
                     echo '<th> Email </th>';
+                    echo '<th> Delete </th>';
+                    echo '<th> Edit </th>';
                     echo '</tr>';
                     foreach($SEARCH as $data){
                         echo '<tr>';
-                        echo '<td>'.$data['name'].'</td>';
+                        echo '<td>'.$data['name'].' </td>';
                         echo '<td> : '.$data['email'].' </td>';
+                        echo '<td><form><button type="submit" name="delete" value="'.$data['id'].'"> delete</button></form></td>';
+                        echo '<td><form><button type="submit" name="edit" value="'.$data['id'].'"> edit</button></form></td>';
                         //echo '<div>'.$data['name']." : ".$data['email'].'</div>';
                         echo '</tr>';
                     }
@@ -40,6 +46,31 @@
                     echo "<div style=\" color:red; \">  failed </dev>";
                 }
             }
+
+            if(isset($_GET['delete'])){
+                    
+                $removeItem = $conn->prepare("DELETE FROM todolist  WHERE id=:ID");
+                $removeItem->bindParam("ID",$_GET['delete']);
+                $removeItem->execute();
+
+                $removeUser = $conn->prepare("DELETE FROM users  WHERE id=:ID");
+                $removeUser->bindParam("ID",$_GET['delete']);
+                if($removeUser->execute()){
+                    echo "<div style=\" color:green; \">  deleted </dev>";
+                    header("Refresh: 1 ; url=search.php");
+                }else{
+                    echo "<div style=\" color:red; \">  failed".$removeItem->errorInfo()." </dev>";
+                }
+            }
+
+            if(isset($_GET['edit'])){
+                $_SESSION['user_id'] =$_GET['edit'];
+                header("Location:edit_user.php",true);
+            }
+
+
+
+
         }else{
             session_unset();
             session_destroy();
